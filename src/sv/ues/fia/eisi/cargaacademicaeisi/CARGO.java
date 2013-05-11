bc5c3072e0a5b77ad80876c93ff87a4fe852623d
DarkLord <mariosanchezsv87@gmail.com>
2013-05-11 16:09:20 -0600

    clases cargo y docente

--- src/sv/ues/fia/eisi/cargaacademicaeisi/CARGO.java
+++ src/sv/ues/fia/eisi/cargaacademicaeisi/CARGO.java
@@ -1,5 +1,32 @@
 package sv.ues.fia.eisi.cargaacademicaeisi;
 
 public class CARGO {
+	private String idcargo;
+	private String nom_cargo;
+
+	public CARGO() {
+	}
+
+	public CARGO(String idcargo, String nom_cargo) {
+		super();
+		this.idcargo = idcargo;
+		this.nom_cargo = nom_cargo;
+	}
+
+	public String getIdcargo() {
+		return idcargo;
+	}
+
+	public void setIdcargo(String idcargo) {
+		this.idcargo = idcargo;
+	}
+
+	public String getNom_cargo() {
+		return nom_cargo;
+	}
+
+	public void setNom_cargo(String nom_cargo) {
+		this.nom_cargo = nom_cargo;
+	}
 
 }
--- src/sv/ues/fia/eisi/cargaacademicaeisi/DOCENTE.java
+++ src/sv/ues/fia/eisi/cargaacademicaeisi/DOCENTE.java
@@ -1,5 +1,94 @@
 package sv.ues.fia.eisi.cargaacademicaeisi;
 
 public class DOCENTE {
+	private String iddocente;
+	private String idcontrato;
+	private String nombre;
+	private String apellido;
+	private String grado_acad;
+	private String correo;
+	private int horas_asig;
+	private String telefono;
+
+	public DOCENTE() {
+	}
+
+	public DOCENTE(String iddocente, String idcontrato, String nombre,
+			String apellido, String grado_acad, String correo, int horas_asig,
+			String telefono) {
+		super();
+		this.iddocente = iddocente;
+		this.idcontrato = idcontrato;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.grado_acad = grado_acad;
+		this.correo = correo;
+		this.horas_asig = horas_asig;
+		this.telefono = telefono;
+	}
+
+	public String getIddocente() {
+		return iddocente;
+	}
+
+	public void setIddocente(String iddocente) {
+		this.iddocente = iddocente;
+	}
+
+	public String getIdcontrato() {
+		return idcontrato;
+	}
+
+	public void setIdcontrato(String idcontrato) {
+		this.idcontrato = idcontrato;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPellido() {
+		return apellido;
+	}
+
+	public void setPellido(String pellido) {
+		this.apellido = pellido;
+	}
+
+	public String getGrado_acad() {
+		return grado_acad;
+	}
+
+	public void setGrado_acad(String grado_acad) {
+		this.grado_acad = grado_acad;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public int getHoras_asig() {
+		return horas_asig;
+	}
+
+	public void setHoras_asig(int horas_asig) {
+		this.horas_asig = horas_asig;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 
 }
